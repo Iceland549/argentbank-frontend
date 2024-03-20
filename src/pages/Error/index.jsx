db491714a7b47276c12284b9ca544c57be720491
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-03-20 15:19:18 +0100

    Add Error function

---
+++ src/pages/Error/index.jsx
@@ -0,0 +1,18 @@
+// Error
+import './public/css/main.css'
+import { Link } from 'react-router-dom';
+
+
+function Error() {
+  return (
+    <div className='error-container'>
+      <h1>404</h1>
+      <p>Oups ! La page que <br/> vous demandez n&apos;exite pas.</p>
+      <Link to={`/`}>
+        <p className='error-link'> Retourner sur la page d&apos;acceuil</p>
+      </Link>
+    </div>
+  );
+}
+
+export default Error;
