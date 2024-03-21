abd581e68b5a8a8ac80d74421f4bebc6f8feeea3
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-03-21 08:38:24 +0100

    Add Home function

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import HeroSection from '../../components/HeroSection';
+import FeaturesSection from '../../components/FeaturesSection';
+
+function Home() {
+  return (
+    <div>
+      <HeroSection /> 
+      <FeaturesSection /> 
+    </div>
+  );
+}
+
+export default Home;
