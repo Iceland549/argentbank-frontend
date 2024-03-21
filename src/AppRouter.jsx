a218a2af6d7cf9512c2d64db831764097df084f2
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-03-21 14:04:10 +0100

    delete router from AppRouter.jsx

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,5 +1,5 @@
 // AppRouter.jsx
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
@@ -8,14 +8,12 @@ import Error404 from './pages/Error';
 
 function AppRouter() {
   return (
-    <Router>
-        <Routes>
-            <Route path="/" element={<Layout><Home /></Layout>} />
-            <Route path="/SignIn" element={<Layout><SignIn/></Layout>} />
-            <Route path="/User" element={<Layout><User /></Layout>} />
-            <Route path="*" element={<Layout><Error404 /></Layout>} />
-        </Routes>
-    </Router>
+      <Routes>
+          <Route path="/" element={<Layout><Home /></Layout>} />
+          <Route path="/SignIn" element={<Layout><SignIn/></Layout>} />
+          <Route path="/User" element={<Layout><User /></Layout>} />
+          <Route path="*" element={<Layout><Error404 /></Layout>} />
+      </Routes>
   );
 }
 
