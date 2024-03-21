7e26b8ce805ace23378bd922cbc1a6ee744c2177
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-03-21 14:06:11 +0100

    modify AppRouter into App

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// AppRouter.jsx
+import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import User from './pages/User';
+import Error404 from './pages/Error';
+
+function App() {
+  return (
+      <Routes>
+          <Route path="/" element={<Layout><Home /></Layout>} />
+          <Route path="/SignIn" element={<Layout><SignIn/></Layout>} />
+          <Route path="/User" element={<Layout><User /></Layout>} />
+          <Route path="*" element={<Layout><Error404 /></Layout>} />
+      </Routes>
+  );
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import AppRouter from './AppRouter'
+import App from './App'
 import './css/main.css';
 import store from './store';
 
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <AppRouter />
+        <App />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
