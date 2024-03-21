116371fe206586944ccf1d43c2f2c2b1af95999c
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-03-21 12:34:38 +0100

    Modify const userSlice into export const

---
+++ src/pages/User/UserSlice.jsx
@@ -0,0 +1,88 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+  isLoading: false,
+  error: null,
+};
+
+export const loginUser = createAsyncThunk(
+  'user/loginUser',
+  async (loginData, thunkAPI) => {
+    try {
+      const response = await fetch('http://locahost:3001/api/user/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(loginData),
+      });
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue({ error: error.message });
+    }
+  }
+);
+
+export const signupUser = createAsyncThunk(
+  'user/signupUser',
+  async (signupData, thunkAPI) => {
+    try {
+      const response = await fetch('http://locahost:3001/api/user/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(signupData),
+      });
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue({ error: error.message });
+    }
+  }
+);
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUser(state, action) {
+      state.user = action.payload;
+    },
+    clearUser(state) {
+      state.user = null;
+    },
+  },
+  extraReducers: {
+    [loginUser.pending]: (state) => {
+      state.isLoading = true;
+      state.error = null;
+    },
+    [loginUser.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.user = action.payload;
+    },
+    [loginUser.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload.error;
+    },
+    [signupUser.pending]: (state) => {
+      state.isLoading = true;
+      state.error = null;
+    },
+    [signupUser.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.user = action.payload;
+    },
+    [signupUser.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload.error;
+    },
+  },
+});
+
+export const { setUser, clearUser } = userSlice.actions;
+
+export default userSlice.reducer;
