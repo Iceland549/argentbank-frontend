10394138c92baf8365d1202d869d572b9e0e63be
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-03-21 08:37:51 +0100

    Add user function

---
+++ src/pages/User/index.jsx
@@ -0,0 +1,11 @@
+import UserAccount from "../../components/UserAccount";
+
+function User() {
+    return (
+        <div>
+            <UserAccount />
+        </div>
+    );
+}
+
+export default User;
