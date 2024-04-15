d7440c8cf4b82adb8e7717ed128dfe240a1e7cf1
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-04-15 14:50:46 +0200

    Delete 'try' from handleSignIn of SignInForm function

--- src/components/SignInForm/index.jsx
+++ src/components/SignInForm/index.jsx
@@ -13,38 +13,15 @@ function SignInForm() {
   const error = useSelector(state => state.user.error);
 
   useEffect(() => {
-    console.log("Token:", token);
     if (token) {
       navigate('/User');
-      console.log('Redirecting to user page...');
     }
   }, [token, navigate]);
-  console.log('Token:', token);
 
 
   const handleSignIn = async (e) => {
     e.preventDefault();   
-    console.log('Submitting form with email:', email, 'and password:', password);
     dispatch(loginUser({ email, password }));
-    
-    try {
-      const loginData = {
-        email: email,
-        password: password
-      };
-      console.log('Login data:', loginData);
-      await dispatch(loginUser(loginData));
-      console.log('Login request sent.');
-      console.log('Login request completed successfully.');
-      
-      if (error) {
-        console.log('Error:', error);
-        console.error('Authentication failed:', error);
-        console.log('Token after dispatch:', token);
-      }
-    } catch (error) {
-      console.error('Failed to sign in:', error);
-    }
   };
 
   return (
@@ -67,6 +44,7 @@ function SignInForm() {
           </div>
           <button type='submit' className="sign-in-button">Sign In</button>
         </form>
+        <p>{error}</p>
       </section>
     </main>
   );
