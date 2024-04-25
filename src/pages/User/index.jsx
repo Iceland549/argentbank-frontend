0e1100a7146aa1deec0c4169f3bb9ca61e9ce91f
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-04-25 13:58:09 +0200

    Delete selectUser and Add handleSaveName to User function

--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,19 +1,15 @@
 import '../../css/main.css'
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectUser } from '../../selector';
 import EditNameForm from '../../components/EditNameForm';
 import Account from '../../components/Account';
 import { useNavigate } from 'react-router-dom';
 import { getUser } from '../../slices/UserSlice';
 
 function User() {
-    const user = useSelector(selectUser);
-    const { firstName, lastName } = useSelector(state => state.user);
+    const { firstName, lastName, token } = useSelector(state => state.user);
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    const token = useSelector(state => state.user.token);
-    console.log('User data:', user);
     const [isEditName, setIsEditName] = useState(false);
 
     useEffect(() => {
@@ -28,16 +24,20 @@ function User() {
         setIsEditName(!isEditName);
     }
 
+    const handleSaveName = () => {
+        setIsEditName(false);
+
+    }
     return (
         <main className="main bg-dark" style={{ height: token ? 'unset' : 'initial' }}>
             <div className="header">
                 {!isEditName && (
                     <>
-                        <h1>Welcome back<br />{user ? `${firstName} ${lastName}` : 'Guest'}!</h1>
+                        <h1>Welcome back<br />{firstName && lastName ? `${firstName} ${lastName}` : 'Guest'}!</h1>
                         <button className="edit-button" onClick={handleEditNameClick}>Edit Name</button>
                     </>
                 )}
-                {isEditName && <EditNameForm onCancel={handleEditNameClick} />}
+                {isEditName && <EditNameForm onSave={handleSaveName} onCancel={handleEditNameClick} />}
             </div>
             <h2 className="sr-only">Accounts</h2>
             <Account title="Argent Bank Checking (x8349)" amount="$2,082.79" description="Available Balance" />
