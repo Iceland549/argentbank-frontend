fecfd6f4463566c9a6773f8059fd6e2c1558d857
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-04-30 08:49:22 +0200

    Delete last modification of selector

--- src/selector.js
+++ src/selector.js
@@ -2,6 +2,17 @@ import { createSelector } from '@reduxjs/toolkit';
 
 export const selectUser = createSelector(
   state => state.user,
-  user => user
+  user => {
+    console.log("User state:", user);
+    return {
+      email: user.email,
+      password: user.password,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      userName: user.userName,
+      isAuthenticated: user.isAuthenticated,
+      token: user.token,
+    };
+  }
 );
   
