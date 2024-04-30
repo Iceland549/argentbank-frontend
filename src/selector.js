002f3f05542a3ea44c394ec534aa410816891935
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-04-30 08:21:50 +0200

    Modify selectUser function

--- src/selector.js
+++ src/selector.js
@@ -2,17 +2,6 @@ import { createSelector } from '@reduxjs/toolkit';
 
 export const selectUser = createSelector(
   state => state.user,
-  user => {
-    console.log("User state:", user);
-    return {
-      email: user.email,
-      password: user.password,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      userName: user.userName,
-      isAuthenticated: user.isAuthenticated,
-      token: user.token,
-    };
-  }
+  user => user
 );
   
