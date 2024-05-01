36d16809e95664b9727e63b80ca15144cffdcf1e
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-05-01 12:03:15 +0200

    Create FeatureItem component

---
+++ src/components/FeatureItem/index.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line react/prop-types
+function FeatureItem({ icon, title, description }) {
+    return (
+        <div className="feature-item">
+            <img src={icon} alt="Feature Icon" className="feature-icon" />
+            <h3 className="feature-item-title">{title}</h3>
+            <p>{description}</p>
+        </div>
+    );
+}
+
+export default FeatureItem;
